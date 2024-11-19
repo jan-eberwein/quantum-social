f381d68f16de0b1800d02fd213966efd7f069f9d
Jan Eberwein <S2410629003@fhooe.at>
2024-11-19 20:09:56 +0100

    Auth + Root Layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import './globals.css';
 import SigninForm from './_auth/forms/SigninForm';
 import { Home } from './_root/pages';
 import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* PUBLIC ROUTES - SIGNED OUT */}
-        <Route path="/sign-in" element={<SigninForm />} />
-        <Route path="/sign-un" element={<SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-un" element={<SignupForm />} />
+        </Route>
         {/* PRIVATE ROUTES - SIGNED IN */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   )
