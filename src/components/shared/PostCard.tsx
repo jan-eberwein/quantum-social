280362707c04500cb2b2faa6a6536b17e9bb1157
Jan Eberwein <S2410629003@fhooe.at>
2024-11-22 17:44:24 +0100

    adding "|" seperator for post

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -36,6 +36,7 @@ const PostCard = ({ post }: PostCardProps) => {
               <p className="subtle-semibold lg:small-regular">
                 {formatDateTime(post.$createdAt)}
               </p>
+              |
               <p className="subtle-semibold lg:small-regular">
                 {post.location}
               </p>
