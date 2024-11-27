d218062799b11cae5d69152c5db685965dc026da
Jan Eberwein <S2410629003@fhooe.at>
2024-11-27 18:33:37 +0100

    remove styling error for GridPostList

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -21,27 +21,30 @@ const GridPostList = ({
     <ul className="grid-container">
       {posts.map((post) => (
         <li key={post.$id} className="relative min-w-80 h-80">
-          {post.caption}
-          <Link to={`/post/${post.$id}`} className="gird-post_link">
+          <Link to={`/posts/${post.$id}`} className="grid-post_link">
             <img
               src={post.imageUrl}
-              alt={post.caption}
+              alt="post"
               className="h-full w-full object-cover"
             />
           </Link>
+
           <div className="grid-post_user">
             {showUser && (
-              <div className="flex items-center gap-2 justify-center flex-1">
+              <div className="flex items-center justify-start gap-2 flex-1">
                 <img
-                  src={post.creator.imageUrl}
+                  src={
+                    post.creator.imageUrl ||
+                    "/assets/icons/profile-placeholder.svg"
+                  }
                   alt="creator"
-                  className="h-8 w-8 rounded-full"
+                  className="w-8 h-8 rounded-full"
                 />
                 <p className="line-clamp-1">{post.creator.name}</p>
               </div>
             )}
+            {showStats && <PostStats post={post} userId={user.id} />}
           </div>
-          {showStats && <PostStats post={post} user={user.id} />}
         </li>
       ))}
     </ul>
