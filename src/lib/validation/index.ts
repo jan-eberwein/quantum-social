a8aeb29000af43c59a0236bbcd349294d20e950b
Johannes Eder <s2110238010@students.fh-hagenberg.at>
2024-11-28 22:27:22 +0100

    Fixed File Validation in Post Create

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -22,8 +22,10 @@ export const SigninValidation = z.object({
 
 export const PostValidation = z.object({
   caption: z.string().min(5, { message: "Minimum 5 characters." }).max(2200, { message: "Maximum 2,200 caracters" }),
-  file: z.custom<File[]>(),
-  location: z.string().min(1, { message: "This field is required" }).max(1000, { message: "Maximum 1000 characters." }),
+  file: z
+      .custom<File[]>((files) => Array.isArray(files) && files.length > 0, {
+        message: "At least one file is required.",
+      }),  location: z.string().min(1, { message: "This field is required" }).max(1000, { message: "Maximum 1000 characters." }),
   tags: z.string(),
 });
 
