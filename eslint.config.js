55d825690bb7ce76dcaaeeb80108c5179603d1e3
Jan Eberwein <S2410629003@fhooe.at>
2024-11-28 22:20:23 +0100

    Adding Jest for Testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,30 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import jest from 'eslint-plugin-jest';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'plugin:jest/recommended',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      jest,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -25,4 +34,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
