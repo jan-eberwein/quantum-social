3f7e52a09d82e90bab86f6e7f3e7f223d120cd00
Jan Eberwein <S2410629003@fhooe.at>
2024-12-05 14:57:51 +0100

    adding Vercel Analytics

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,8 +1,9 @@
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
-import React from "react";
 import { Outlet } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/next';
+
 
 const RootLayout = () => {
   return (
@@ -12,6 +13,7 @@ const RootLayout = () => {
 
       <section className="flex flex-1 h-full">
         <Outlet />
+        <Analytics />
       </section>
 
       <Bottombar />
